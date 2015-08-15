fcaa5d1ef2598a18f858e1e76acf069b4d4200e7
Johnny Vu <jvu@guthy-renker.com>
2015-08-15 16:55:10 -0700

    init git MEAN Foundation FontAwesome

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('pmeggApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
